e21847caee80e52d13f24276a88a8e984b5fc1a8
shafeeque <shafeeqsha06@gmail.com>
2024-11-10 19:08:45 +0530

    feat(comment-creation): added comment creation ui component

---
+++ client/src/components/CommentCreate.tsx
@@ -0,0 +1,37 @@
+import axios from "axios";
+import { useState } from "react";
+
+type Props = {
+  postId: string;
+};
+
+const CommentCreate = ({ postId }: Props) => {
+  const [content, setContent] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    await axios.post( `http://localhost:4001/posts/${postId}/comments`, {
+      postId,
+    });
+
+    setContent("");
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>New Comment</label>
+          <input
+            type="text"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          />
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default CommentCreate;
