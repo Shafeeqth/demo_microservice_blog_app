163b0fa296c815eea713ab868e0914ab536bd3b2
shafeeque <shafeeqsha06@gmail.com>
2024-11-11 15:46:02 +0530

    feat(comment): added new api end point to listen for CommentCreated event

--- comments/index.js
+++ comments/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const {randomBytes} = require('node:crypto');
+const { randomBytes } = require('node:crypto');
 const commentsByPostId = require('./data.db');
 const app = express();
-const cors = require('cors')
+const cors = require('cors');
+const axios = require('axios');
 
 
 app.use(express.json());
@@ -11,19 +12,34 @@ app.use(cors());
 
 app.get("/posts/:id/comments", (req, res) => {
     res.send(commentsByPostId[req.params.id] || []);
-    
+ 
 });
 
-app.post("/posts/:id/comments", (req, res) => { 
+app.post("/posts/:id/comments", async (req, res) => {
     const commentId = randomBytes(4).toString("hex");
-    const {content}  = req.body;
-    
+    const { content } = req.body;
+
     const comments = commentsByPostId[req.params.id] || [];
-    comments.push({id: commentId, content});
+    comments.push({ id: commentId, content });
     commentsByPostId[req.params.id] = comments;
-    res.status(201).json({comments})
+
+    await axios.post('http://localhost:4005/events', {
+        type: "CommentCreated",
+        data: {
+            id: commentId,
+            content,
+            postId: req.params.id,
+        }
+    });
+    res.status(201).json({ comments })
 
 });
 
+app.post("/events", (req, res) => {
+    console.log('Event recieved', req.body.type);
 
-app.listen(4001, () => console.log("server linsten to http://localhost:"+4001))
+    res.send({});
+});
+
+
+app.listen(4001, () => console.log("server linsten to http://localhost:" + 4001))
