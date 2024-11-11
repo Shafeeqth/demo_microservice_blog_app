662858be395ff69987ca0581cdbc957041fa3241
shafeeque <shafeeqsha06@gmail.com>
2024-11-11 15:47:13 +0530

    feat(posts): added new api end point to listen for PostCreated event

--- posts/index.js
+++ posts/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const {randomBytes} = require("crypto");
-const cors = require('cors')
+const { randomBytes } = require("crypto");
+const cors = require('cors');
+const axios = require('axios');
 
 // Import post data
 // - in memory - data may loss for refreshs
@@ -17,17 +18,30 @@ app.get("/posts", (req, res) => {
 
 });
 
-app.post("/posts", (req, res) => {
+app.post("/posts", async (req, res) => {
     const id = randomBytes(4).toString("hex");
-    const {title} = req.body;
+    const { title } = req.body;
 
     posts[id] = {
         id: id,
         title: title
     }
+    await axios.post('http://localhost:4005/events', {
+        type: "PostCreated",
+        data: {
+            id,
+            title
+        }
+    })
     res.status(201).send(posts[id]);
 
-});
+});  
+
+app.post("/events", (req, res) => {
+    console.log('Event recieved', req.body.type);
+
+    res.send({});
+})
 
 app.listen(4000, () => {
     console.log("Listening on http://localhost:4000");
