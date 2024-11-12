3436441223ca0491b1c198fd5bf7c57da0857777
shafeeque <shafeeqsha06@gmail.com>
2024-11-12 16:08:11 +0530

    fix(event-bus): added error handling try catch methods to handle api errors

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,13 +5,19 @@ const axios = require('axios');
 const app = express();
 app.use(express.json());
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
     const event = req.body;
-    axios.post("http://localhost:4000/events", event);
-    axios.post("http://localhost:4001/events", event);
-    axios.post("http://localhost:4002/events", event);
+    try {
+        await axios.post("http://localhost:4000/events", event);
+        await axios.post("http://localhost:4001/events", event);
+        await axios.post("http://localhost:4002/events", event);
+        await axios.post("http://localhost:4003/events", event);
+    } catch (error) {
+        console.error(error)
 
-    res.status(200).send({ status: "OK" });
+    }
+
+    return res.status(200).send({ status: "OK" });
 });
 
 app.listen(4005, () => console.log("listening at 4005"))
