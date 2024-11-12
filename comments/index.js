2d78f31bd32ca51b015de1e2f44bc3b6ecf2e8b7
shafeeque <shafeeqsha06@gmail.com>
2024-11-12 16:11:41 +0530

    feat(comments): added logic to update moderated comments - added new post request which trigger CommedUpdated event

--- comments/index.js
+++ comments/index.js
@@ -12,7 +12,7 @@ app.use(cors());
 
 app.get("/posts/:id/comments", (req, res) => {
     res.send(commentsByPostId[req.params.id] || []);
- 
+
 });
 
 app.post("/posts/:id/comments", async (req, res) => {
@@ -20,7 +20,7 @@ app.post("/posts/:id/comments", async (req, res) => {
     const { content } = req.body;
 
     const comments = commentsByPostId[req.params.id] || [];
-    comments.push({ id: commentId, content });
+    comments.push({ id: commentId, content, status: 'pending' });
     commentsByPostId[req.params.id] = comments;
 
     await axios.post('http://localhost:4005/events', {
@@ -29,16 +29,35 @@ app.post("/posts/:id/comments", async (req, res) => {
             id: commentId,
             content,
             postId: req.params.id,
+            status: "pending"
         }
     });
-    res.status(201).json({ comments })
+    return res.status(201).json({ comments })
 
 });
 
-app.post("/events", (req, res) => {
-    console.log('Event recieved', req.body.type);
+app.post("/events", async (req, res) => {
+    const { type , data} = req.body;
 
-    res.send({});
+    if (type === "CommentModerated") {
+        const { id, postId, status, content } = data;
+
+        const comments = commentsByPostId[postId];
+        const comment = comments.find(comment => comment.id === id);
+        comment.status = status;
+        
+        await axios.post('http://localhost:4005/events', {
+            type: "CommentUpdated",
+            data: {
+                id,
+                content,
+                status,
+                postId
+            }
+        })
+    }
+
+   return  res.send({});
 });
 
 
